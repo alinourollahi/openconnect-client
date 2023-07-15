6c6e2bd3fb5fb773ad2a1680c61ee30cfd731daf
ali.nourollahi <alinourollahi777@gmail.com>
2023-07-15 12:36:27 +0000

    Add openconnect-client.sh file

---
+++ openconnect-client.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+function start() {
+  echo <<pass>> | sudo openconnect <<ip>> --user=<<user-name>>  -b --no-dtls --passwd-on-stdin --servercert <<servercert>> >> /dev/null 2>& 1
+}
+
+
+function stop() {
+  sudo kill -9 `ps aux | grep openconnect | awk '{print $2}'` >> /dev/null 2>& 1
+}
+
+
+if [[ $1 = "start" ]]
+then
+	start
+elif [[ $1 = "stop" ]]
+then
+	stop
+else
+    echo "Usage: opc start          start openconnect"
+	echo "   or: opc stop           stop openconnect"
+	exit -1
+fi
