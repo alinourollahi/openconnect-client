deac8c5cb8166d8c6b9a86213f95acccac311aa8
ali.nourollahi <alinourollahi777@gmail.com>
2023-07-29 10:11:10 +0330

    Add restart function

--- openconnect-client.sh
+++ openconnect-client.sh
@@ -1,14 +1,29 @@
 #!/bin/bash
 
 function start() {
-  echo "<pass>" | sudo openconnect "<ip>" --user="<user-name>"  -b --no-dtls --passwd-on-stdin --servercert "<servercert>" >> /dev/null 2>& 1
+  echo "<pass>" | openconnect "<ip>" --user="<user-name>"  -b --no-dtls --passwd-on-stdin --servercert "<servercert>" >> /dev/null 2>& 1
 }
 
 
 function stop() {
-  sudo kill -9 `ps aux | grep openconnect | awk '{print $2}'` >> /dev/null 2>& 1
+  kill -9 `ps aux | grep openconnect | awk '{print $2}'` >> /dev/null 2>& 1
+  dhclient 
 }
 
+function restart() {
+	stop
+	main_interface=$(ip route | grep default | awk '{print $5}')
+	ip link set dev $main_interface down
+	ip link set dev $main_interface up
+	sleep 1
+	start
+
+}
+
+if [ "$EUID" -ne 0 ]; then
+  echo "Please run this script as root (or using sudo)." >&2
+  exit 1
+fi
 
 if [[ $1 = "start" ]]
 then
@@ -16,6 +31,9 @@ then
 elif [[ $1 = "stop" ]]
 then
 	stop
+elif [[ $1 = "restart" ]]
+then
+	restart
 else
     echo "Usage: opc start          start openconnect"
 	echo "   or: opc stop           stop openconnect"
